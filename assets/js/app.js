d1f49cc3999efeec7424fe8e3b3411ed47c2d15f
Thibault LASSIAZ <tlassiaz@norsys.fr>
2018-11-14 13:49:47 +0100

    refacto public directory

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+const $ = require('jquery');
+require('bootstrap/scss/bootstrap.scss');
+require('font-awesome/css/font-awesome.css');
+require('../css/app.css');
+require('./test.js');
+require('bootstrap');
